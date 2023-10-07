b14f57b1334c4f6d742df796a645cd4c7134230c
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-10-08 02:46:30 +0500

    Skip errors

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,4 +1,5 @@
 import {Bot, InputFile} from "grammy";
+import {autoRetry} from "@grammyjs/auto-retry";
 import {autoQuote} from "@roziscoding/grammy-autoquote";
 
 const isNode = typeof EdgeRuntime !== "string";
@@ -12,27 +13,39 @@ export const {
 
 export const bot = new Bot(token);
 
+bot.api.config.use(autoRetry({
+    retryOnInternalServerErrors: true,
+    maxRetryAttempts: 1,
+    maxDelaySeconds: 10
+}));
+
 bot.use(autoQuote);
 
 const safe = bot.errorBoundary(console.error);
 
 safe.on(":sticker:is_animated", async ctx => {
-    await ctx.replyWithChatAction("upload_document");
+    void ctx.replyWithChatAction("upload_document").catch(console.error);
     const file = await convert(await ctx.getFile());
     return ctx.replyWithDocument(file);
 });
 
 safe.on("::custom_emoji", async ctx => {
-    await ctx.replyWithChatAction("upload_document");
+    void ctx.replyWithChatAction("upload_document").catch(console.error);
     const stickers = await ctx.getCustomEmojiStickers();
     const animated = stickers.filter(({is_animated} = {}) => is_animated);
     if (!animated.length) return ctx.reply("Send animated (vector) emoji");
-    return Promise.all(animated.map(async ({file_id}, index) => {
-        const file = await ctx.api.getFile(file_id);
-        const filename = [++index, "svg"].join(".");
-        const svg = await convert(file, filename);
-        return ctx.replyWithDocument(svg);
-    }));
+    for (const index in animated) {
+        try {
+            void ctx.replyWithChatAction("upload_document").catch(console.error);
+            const {file_id} = animated[index];
+            const file = await ctx.api.getFile(file_id);
+            const filename = [parseInt(index) + 1, "svg"].join(".");
+            const svg = await convert(file, filename);
+            await ctx.replyWithDocument(svg);
+        } catch (e) {
+            console.error(e);
+        }
+    }
 });
 
 safe.on("msg", ctx => ctx.reply(`Send animated sticker or custom emoji`));
